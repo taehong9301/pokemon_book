740cb1f49ae7820f9a2448f7be127c1365bb07b2
taehong.kim <7538518@naver.com>
2019-12-11 00:11:04 +0900

    default

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import MainScreen from "./screen/MainScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <React.Fragment>
+      <MainScreen></MainScreen>
+    </React.Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
